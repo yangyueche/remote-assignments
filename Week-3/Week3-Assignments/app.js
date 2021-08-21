3276c576b78c5f07e7385f428b15b42c0530bbbf
yangyueche <yangyueche@gmail.com>
2021-08-21 16:20:20 +0800

    Add week3 Assignments

---
+++ Week-3/Week3-Assignments/app.js
@@ -0,0 +1,61 @@
+const express = require('express');//express 套件載入到程式中
+const cookieParser = require('cookie-parser');
+
+const app = express();//產生 Express Application 物件
+
+app.use(cookieParser());
+app.set('view engine', 'pug');
+
+
+//<Assignment 1>
+app.get('/', (req, res) => {
+  res.render("index")
+});
+
+
+
+//<Assignment 2>
+app.get('/data', (req, res) => {
+  if (!req.query.number) {//檢查變數是否存在
+    res.send("<h1>Lack of Parameter<h1>");
+  } else if (isNaN(req.query.number)) {
+    res.send("<h1>Wrong Parameter<h1>");
+  } else if (req.query.number) {
+    const result = ((1 + Number(req.query.number)) * Number(req.query.number)) / 2;
+    res.send(`<h1>${result}<h1>`);
+  }
+})
+
+
+
+//<Assignment 3>
+app.use(express.static('public'));//如果要提供影像、CSS 檔案和 JavaScript 檔案等之類的靜態檔案，請使用 Express 中的 express.static 內建中介軟體函數。
+
+app.get('/sum.html', (req, res) => {
+  if (!req.query.number) {//檢查變數是否存在
+    res.send("<h1>Lack of Parameter<h1>");
+  } else if (isNaN(req.query.number)) {
+    res.send("<h1>Wrong Parameter<h1>");
+  } else if (req.query.number) {
+    const result = ((1 + Number(req.query.number)) * Number(req.query.number)) / 2;
+    res.send(`<h1>${result}<h1>`);
+  }
+})
+
+
+//<Assignment 4>
+app.get('/myName', (req, res) => {
+  if (req.cookies.username) {
+    res.render('myName', { name: req.cookies.username });
+  } else {
+    if (req.query.username) {
+      res.cookie('username', req.query.username);
+      res.redirect('/myName');
+      //為什麼不能用 res.render('myName', { name: req.cookies.username });
+    } else {
+      res.render('myName');
+    }
+  }
+})
+
+app.listen(3000);
